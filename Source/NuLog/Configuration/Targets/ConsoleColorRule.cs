27badf62cb41e7d3e8fec015f05de97bc3c262c8
ivanpointer <ivan@pointerplace.us>
2014-10-05 19:41:39 -0700

    Cleanup and comments

--- Source/NuLog/Configuration/Targets/ConsoleColorRule.cs
+++ Source/NuLog/Configuration/Targets/ConsoleColorRule.cs
@@ -1,14 +1,32 @@
-﻿using System;
+﻿/*
+ * Author: Ivan Andrew Pointer (ivan@pointerplace.us)
+ * Date: 10/5/2014
+ * License: MIT (http://opensource.org/licenses/MIT)
+ * GitHub: https://github.com/ivanpointer/NuLog
+ */
+using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 
 namespace NuLog.Configuration.Targets
 {
+    /// <summary>
+    /// Represents a color rule 
+    /// </summary>
     public class ConsoleColorRule
     {
+        /// <summary>
+        /// The tags that match this color rule
+        /// </summary>
         public ICollection<string> Tags { get; set; }
-        public ConsoleColor? Color { get; set; }
+
+        /// <summary>
+        /// The foreground color applied by this rule
+        /// </summary>
+        public ConsoleColor? ForegroundColor { get; set; }
+
+        /// <summary>
+        /// The background color applied by this rule
+        /// </summary>
         public ConsoleColor? BackgroundColor { get; set; }
     }
 }
--- Source/NuLog/Configuration/Targets/ConsoleTargetConfig.cs
+++ Source/NuLog/Configuration/Targets/ConsoleTargetConfig.cs
@@ -1,60 +1,96 @@
-﻿using Newtonsoft.Json.Linq;
+﻿/*
+ * Author: Ivan Andrew Pointer (ivan@pointerplace.us)
+ * Date: 10/5/2014
+ * License: MIT (http://opensource.org/licenses/MIT)
+ * GitHub: https://github.com/ivanpointer/NuLog
+ */
+using Newtonsoft.Json.Linq;
 using System;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace NuLog.Configuration.Targets
 {
+    /// <summary>
+    /// The configuration that represents the console target
+    /// </summary>
     public class ConsoleTargetConfig : LayoutTargetConfig
     {
+        #region Constants
+        // Tokens
+        private const string ColorRulesTokenName = "colorRules";
+        private const string TagsTokenName = "tags";
+        private const string ForeColorTokenName = "foreColor";
+        private const string BackColorTokenName = "backColor";
+
+        // Functional Values
+        private const string TraceConfigCategory = "config";
+
+        #endregion
+
+        /// <summary>
+        /// The list of color rules associated to this configuration
+        /// </summary>
         public ICollection<ConsoleColorRule> ColorRules { get; set; }
 
+        /// <summary>
+        /// Instantiates a base console target config
+        /// </summary>
         public ConsoleTargetConfig()
             : base(synchronous: true)
         {
             ColorRules = new List<ConsoleColorRule>();
         }
 
+        /// <summary>
+        /// Instantiates a console target config using the passed JSON token
+        /// </summary>
+        /// <param name="jToken">The JSON token to parse for this console target config</param>
         public ConsoleTargetConfig(JToken jToken)
             : base(jToken, synchronous: true)
         {
+            // Initialize the list of color rules
             ColorRules = new List<ConsoleColorRule>();
 
+            // If a JSON configuration was provided
             if (jToken != null)
             {
-                var colorRulesToken = jToken["colorRules"];
+                // Get a hold of the color rules in the JSON if they are provided
+                var colorRulesToken = jToken[ColorRulesTokenName];
                 if (colorRulesToken != null && colorRulesToken.Type == JTokenType.Array)
                 {
+                    // Prepare to parse the rules
                     var colorRulesTokens = colorRulesToken.Children();
-
                     ConsoleColorRule colorRule;
                     Type ConsoleColorType = typeof(ConsoleColor);
                     JToken tags;
-                    JToken color;
-                    JToken backgroundColor;
+                    JToken foreColorToken;
+                    JToken backColorToken;
 
+                    // Iterate over each of the child JSON tokens
+                    //  in the rules
                     foreach (var colorRuleToken in colorRulesTokens)
                     {
                         try
                         {
+                            // Make sure that the child element is an object
                             if (colorRuleToken.Type == JTokenType.Object)
                             {
+                                // And if so, parse out the parts
                                 colorRule = new ConsoleColorRule();
 
-                                tags = colorRuleToken["tags"];
+                                tags = colorRuleToken[TagsTokenName];
                                 if (tags != null && tags.Type == JTokenType.Array)
                                     colorRule.Tags = tags.Values<string>().ToList();
 
-                                color = colorRuleToken["color"];
-                                if (color != null && color.Type == JTokenType.String)
-                                    colorRule.Color = (ConsoleColor)Enum.Parse(ConsoleColorType, color.Value<string>());
+                                foreColorToken = colorRuleToken[ForeColorTokenName];
+                                if (foreColorToken != null && foreColorToken.Type == JTokenType.String)
+                                    colorRule.ForegroundColor = (ConsoleColor)Enum.Parse(ConsoleColorType, foreColorToken.Value<string>());
 
-                                backgroundColor = colorRuleToken["background"];
-                                if (backgroundColor != null && backgroundColor.Type == JTokenType.String)
-                                    colorRule.BackgroundColor = (ConsoleColor)Enum.Parse(ConsoleColorType, backgroundColor.Value<string>());
+                                backColorToken = colorRuleToken[BackColorTokenName];
+                                if (backColorToken != null && backColorToken.Type == JTokenType.String)
+                                    colorRule.BackgroundColor = (ConsoleColor)Enum.Parse(ConsoleColorType, backColorToken.Value<string>());
 
                                 ColorRules.Add(colorRule);
                             }
@@ -63,7 +99,7 @@ namespace NuLog.Configuration.Targets
                         {
                             // Just ignore this color rule - colors are not mission critical
                             // Log the failure
-                            Trace.WriteLine(e, Constants.TraceLogger.ConfigCategory);
+                            Trace.WriteLine(e, TraceConfigCategory);
                         }
                     }
                 }
