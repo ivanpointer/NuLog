c80cf881af92174e0db799cb8451843009af672e
Ivan Pointer <ivan@pointerplace.us>
2017-06-24 19:24:14 -0500

    Add test to cover disposal of LogEvents.

---
+++ Take2/NuLog.Tests/Unit/LogEventTests.cs
@@ -0,0 +1,21 @@
+﻿/* © 2017 Ivan Pointer
+MIT License: https://github.com/ivanpointer/NuLog/blob/master/LICENSE
+Source on GitHub: https://github.com/ivanpointer/NuLog */
+
+using NuLog.LogEvents;
+using Xunit;
+
+namespace NuLog.Tests.Unit
+{
+    [Trait("Category", "Unit")]
+    public class LogEventTests
+    {
+        [Fact(DisplayName = "Should_Dispose")]
+        public void Should_Dispose()
+        {
+            // A log event shouldn't blow up on dispose
+            var logEvent = new LogEvent();
+            logEvent.Dispose();
+        }
+    }
+}
